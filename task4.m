5566a5d3acb77edc29515728ef1591ef7a0d019c
mitoand9 <mitoand9@yahoo.it>
2018-05-14 18:01:21 +0200

    lssossksk

--- task4.m
+++ task4.m
@@ -53,3 +53,21 @@ semilogy(w,PSD)
 utsav(1,coeff)=T;
 %  end
 % end
+
+
+% %% task 6
+% clear all
+% 
+% c_s = 0.05;
+% m_s = 0.16;
+% k_s = 0.0632;
+% c_p = 0.8;
+% m_p = 0.16;
+% k_p = 6.32;
+% 
+% A = [-c_s/m_s c_s/m_s -k_s/m_s k_s/m_s; c_s/m_p -(c_s+c_p)/m_p k_s/m_p -(k_s+k_p)/m_p; 1 0 0 0; 0 1 0 0];
+% B = [0 0; c_p/m_p k_p/m_p; 0 0; 0 0];
+% C = [0 0 1 0; 0 0 0 1; 0 0 1 -1];
+% D = [0 0; 0 0; 0 0];
+% 
+% sys = ss(A,B,C,D);
