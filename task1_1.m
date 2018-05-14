f95021ef808e8d0023b5d38325c82b1bd18ed43c
mitoand9 <mitoand9@yahoo.it>
2018-05-14 15:33:17 +0200

    kkkj

--- task1_1.m
+++ task1_1.m
@@ -1,11 +1,11 @@
 %%TASK 1.1
 m_p = 0.16; %kg
-c_p = 0.4; %Ns/m
 k_p = 6.32; %N/m
+c_p = 2*sqrt(k_p*m_p); %Ns/m, 0.4 originally
 c_p1 = 0;
 
 w_n = sqrt(k_p/m_p);
-zeta= c_p/2*sqrt(k_p*m_p);
+zeta = c_p/2*sqrt(k_p*m_p)
 zeta1 = c_p1/2*sqrt(k_p*m_p);
 %%TASK 1.3
 H = tf([0,2*zeta*w_n,w_n^2],[1,2*zeta*w_n,w_n^2]);
@@ -17,7 +17,7 @@ bode(H,H1);
 
 %% task 1.3
 
-t = 1:0.01:10;
+t = 0:0.01:10;
 excitation_sin = 0.05*sin(3*t);
 
 figure()
