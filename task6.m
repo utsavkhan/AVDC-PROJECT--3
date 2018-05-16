a8798d6133a44fb501c2eedece2c31d0681fe1f0
mitoand9 <mitoand9@yahoo.it>
2018-05-16 10:02:23 +0200

    skkssk

--- task6.m
+++ task6.m
@@ -9,11 +9,16 @@ c_p = 0.8;
 m_p = 0.16;
 k_p = 6.32;
 
-A = [-c_s/m_s c_s/m_s -k_s/m_s k_s/m_s; c_s/m_p -(c_s+c_p)/m_p k_s/m_p -(k_s+k_p)/m_p; 1 0 0 0; 0 1 0 0];
 B = [0 0; c_p/m_p k_p/m_p; 0 0; 0 0];
 C = [0 0 1 0; 0 0 0 1; 0 0 1 -1];
 D = [0 0; 0 0; 0 0];
+% B = [0 0; 0 0; 0 0; c_p/m_p k_p/m_p];
+% C = [1 0 0 0];
+% D = 0;
+% 
+% sys = ss(A,B,C,D);
 
-sys = ss(A,B,C,D);
+s = tf('s');
+H = tf(-k_p-c_p*s,k_s+c_s*s+((k_s+c_s*s+m_s*s^2)/(k_s+c_s*s))*(-k_s-c_s*s-k_p-c_p*s-m_p*s^2));
 
-bode(sys)
+bode(H)
