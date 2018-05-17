46f2f596069f4ca9aa2a93593a5b76f08f428670
mitoand9 <mitoand9@yahoo.it>
2018-05-17 14:54:03 +0200

    trap

--- task6.m
+++ task6.m
@@ -21,9 +21,10 @@ H = (-kp-cp*s)/(ks+cs*s+((ks+cs*s+ms*s^2)/(ks+cs*s))*(-ks-cs*s-kp-cp*s-mp*s^2));
 % H = tf(cs*cp*s^2+kp*cs*s+cp*ks*s+ks*kp,(ms*s^2+cs*s+ks)*(mp*s^2+cp*s+kp));
 
 
-% pole(H)
-% 
-% bode(H)
+pole(H)
+
+figure(1)
+bode(H)
 % 
 % step(H)
 
@@ -31,12 +32,8 @@ H = (-kp-cp*s)/(ks+cs*s+((ks+cs*s+ms*s^2)/(ks+cs*s))*(-ks-cs*s-kp-cp*s-mp*s^2));
 t = 0:0.01:10;
 excitation_sin = 0.05*sin(3*t);
 
-% figure()
-% plot(t,excitation_sin)
-
-figure(1)
+figure(2)
 lsim(H,excitation_sin,t) % sine excitation
-hold on;
 
 %% Impulse
 stepinput = 0.05*ones([1 length(t)]);
@@ -44,17 +41,17 @@ for i = 101:length(t)
    stepinput(i) = 0; 
 end
 
-figure(2)
+figure(3)
 lsim(H,stepinput,t) % impulse excitation
-hold on;
 
 %% PSD
-w = 0:25;
+w = 0:0.01:25;
 PSD = (4.028e-7)./((2.88e-4)+(0.68*w.^2)+w.^4);
 Hfreqdomain = freqresp(H,w);
 H = abs(Hfreqdomain(:))';
 PSD = H.^2.*PSD;
 
-figure(3)
+figure(4)
 semilogy(w,PSD)
-hold on
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
