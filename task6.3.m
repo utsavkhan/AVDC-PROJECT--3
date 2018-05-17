785f248a822d4c7814c71be3172c4e4b89771a46
utsavkhan <38136166+utsavkhan@users.noreply.github.com>
2018-05-17 19:49:46 +0200

    1

--- task6.3.m
+++ task6.3.m
@@ -2,16 +2,18 @@
 
 clear all
 
-cs = 0.05;
-ms = 0.16;
-ks = 0.0632;
-cp = 0.8;
-mp = 0.16;
-kp = 6.32;
+c_s = 0.05;
+m_s = 0.16;
+k_s = 0.0632;
+c_p = 0.8;
+m_p = 0.16;
+k_p = 6.32;
+T = 1;
 
-A = [0 1 0 0; -k_s/m_s -c_s/m_s k_s/m_s c_s/m_s; 0 0 0 1; k_s/m_p c_s/m_p -(k_s+k_p)/m_p -(c_s+c_p)/m_p];
-B = [0 0; 0 0; 0 0; c_p/m_p k_p/m_p];
+A = [0 1 0 0; -k_s/m_s -T/m_s k_s/m_s 0; 0 0 0 1; k_s/m_p T/m_p -(k_s+k_p)/m_p -(c_p/m_p)];
+B = [0 0; 0 0; 0 0; k_p/m_p c_p/m_p];
 C = [1 0 0 0];
 D = 0;
 
 sys = ss(A,B,C,D);
+bode(sys)
