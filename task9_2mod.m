96ae4b36167d979b7972172ffcb8461eae9415d1
utsavkhan <38136166+utsavkhan@users.noreply.github.com>
2018-05-21 11:18:49 +0200

    1

--- task9_2mod.m
+++ task9_2mod.m
@@ -21,18 +21,14 @@ excite=1; %%Impulse
 %% active
 
 A=[0 1 0 0;
-    (-k1-k2)/m -cz/m (k1*l1-k2*l2)/m 0;
+    (-k1-k2)/m 0 (k1*l1-k2*l2)/m 0;
     0 0 0 1;
-    (k1*l1-k2*l2)/j 0 (-k1*(l1^2)-k2*(l2^2))/j -cx/j];
-B=[0 0;k1/m k2/m;0 0;-k1*l1/j k2*l2/j];
-C=[1 0 0 0;0 1 0 0;0 0 1 0;0 0 0 1];
-D=[0 0;0 0;0 0;0 0];
-% [num,den]=ss2tf(A,B,C,D,3);
-% bounce=tf(num(1,:),den);
-% pitch=tf(num(3,:),den);
-% bode(bounce,pitch);
+    (k1*l1-k2*l2)/j 0 (-k1*(l1^2)-k2*(l2^2))/j 0];
+B=[0 0 0 0;k1/m k2/m 1/m 1/m;0 0 0 0;-k1*l1/j k2*l2/j -l1/j l2/j];
+C=eye(4);
+D=zeros(4);
 
-sim('task9_2')
+sim('task9_2modified')
 
 figure(1)
 plot(simout1.Time, simout1.Data(:,1));
@@ -45,7 +41,6 @@ plot(simout1.Time, simout1.Data(:,1));
 hold on;
 plot(simout2.Time, simout2.Data(:,1));
 hold on;
-
 
 %% passive
 
@@ -75,4 +70,3 @@ legend('Input','Active System','Passive System');
 figure(2)
 plot(simout2.Time, simout2.Data(:,1));
 hold on;
-
