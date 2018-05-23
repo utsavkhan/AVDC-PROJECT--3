bf1f3a5cf87c9acefc1e2565fe757cb2a3e48df9
mitoand9 <mitoand9@yahoo.it>
2018-05-23 13:41:41 +0200

    m,m,

--- task1_1.m
+++ task1_1.m
@@ -1,18 +1,23 @@
 clc;
 clear all;
 
-%%TASK 1.1
+%% TASK 1.1
 m_p = 0.16; %kg
 c_p = 0.4; %Ns/m
 k_p = 6.32; %N/m
-c_p1 = 0;
+c_p1 = 2.0112;
+c_p2 = 3;
 
 w_n = sqrt(k_p/m_p);
-zeta= c_p/(2*sqrt(k_p*m_p));
+zeta = c_p/(2*sqrt(k_p*m_p));
 zeta1 = c_p1/(2*sqrt(k_p*m_p));
-%%TASK 1.3
-H = tf([0,2*zeta*w_n,w_n^2],[1,2*zeta*w_n,w_n^2])
-H1 = tf([0,2*zeta1*w_n,w_n^2],[1,2*zeta1*w_n,w_n^2])
+zeta2 = c_p2/(2*sqrt(k_p*m_p));
+
+%% TASK 1.3
+
+H = tf([0,2*zeta*w_n,w_n^2],[1,2*zeta*w_n,w_n^2]);
+H1 = tf([0,2*zeta1*w_n,w_n^2],[1,2*zeta1*w_n,w_n^2]);
+H2 = tf([0,2*zeta2*w_n,w_n^2],[1,2*zeta2*w_n,w_n^2]);
 % fr= 0:0.01:100;
 % [mag,phase,wout] = bode(H1,fr);
 % bode(H,H1);
@@ -21,36 +26,46 @@ H1 = tf([0,2*zeta1*w_n,w_n^2],[1,2*zeta1*w_n,w_n^2])
 
 % %% task 1.3
 % 
-%% sine excitation
-t = 0:0.01:10;
-excitation_sin = 0.05*sin(3*t);
+% %% sine excitation
+% t = 0:0.01:10;
+% excitation_sin = 0.05*sin(3*t);
+% 
+% % figure()
+% % plot(t,excitation_sin)
+% 
+% figure(1)
+% lsim(H,excitation_sin,t) % sine excitation
+% hold on;
+% set(findall(gcf,'type','line'), 'LineWidth', 2);
+% 
+% %% Impulse
+% stepinput = 0.05*ones([1 length(t)]);
+% for i = 101:length(t)
+%    stepinput(i) = 0; 
+% end
+% 
+% figure(2)
+% lsim(H,stepinput,t) % impulse excitation
+% hold on;
+% set(findall(gcf,'type','line'), 'LineWidth', 2);
+% 
+% %% PSD
+% w = 0:25;
+% PSD = (4.028e-7)./((2.88e-4)+(0.68*w.^2)+w.^4);
+% Hfreqdomain = freqresp(H,w);
+% H = abs(Hfreqdomain(:))';
+% PSD = H.^2.*PSD;
+% 
+% figure(3)
+% semilogy(w,PSD)
+% hold on;
+% set(findall(gcf,'type','line'), 'LineWidth', 2);
 
-% figure()
-% plot(t,excitation_sin)
+%% Impulse (instantaneous)
 
-figure(1)
-lsim(H,excitation_sin,t) % sine excitation
-hold on;
-set(findall(gcf,'type','line'), 'LineWidth', 2);
- % Impulse
-stepinput = 0.05*ones([1 length(t)]);
-for i = 101:length(t)
-   stepinput(i) = 0; 
-end
-
-figure(2)
-lsim(H,stepinput,t) % impulse excitation
-hold on;
-set(findall(gcf,'type','line'), 'LineWidth', 2);
- %%PSD
-w = 0:25;
-PSD = (4.028e-7)./((2.88e-4)+(0.68*w.^2)+w.^4);
-Hfreqdomain = freqresp(H,w);
-H = abs(Hfreqdomain(:))';
-PSD = H.^2.*PSD;
-
-figure(3)
-semilogy(w,PSD)
-hold on;
-set(findall(gcf,'type','line'), 'LineWidth', 2);
-
+impulse(H)
+hold on
+impulse(H1)
+hold on
+impulse(H2)
+legend('Under Damped','Critically Damped','Over Damped')
